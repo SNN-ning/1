c22ace2d48c4ec6b2a895c69d9be884d146c1412
snn <1535836531@qq.com>
2019-10-11 23:20:02 +0800

    Initial commit

---
+++ src/test/java/junit_text.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class junit_text {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("ипн╢й╣ож");
+	}
+
+}
